134db13e219619d87506ccf5ef9572f036e9e3d0
Adrien Mariez <adrien.mariez@gmail.com>
2017-12-07 16:31:24 +0100

    code qui fonctionne (presque)

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('have fun !');
+// var first_name =  "#first_name";
+// var last_name = "#last_name";
+// var city = "#city";
+
+var objekt218 = {
+	first    : document.getElementById("first_name").value,
+	last   : document.getElementById("last_name").value,
+	city   : document.getElementById("city").value
+};
+
+
+	$('button').click(function(){ 
+		var value_first = $("#first_name").val();
+		var value_last = $("#last_name").val();
+		var value_city = $("#city").val();
+
+		var joliobjet = ('bienvenue à ' + value_first + ' ' + value_last  + ' de ' + value_city + ' . Prenez vos aises !');
+
+		console.log(objekt218);
+
+		$("#username").html(value_first+' '+value_last+' de '+value_city);
+
+	});
